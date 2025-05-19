167e74d88b780fb72acc17b860b85a54c8f324c3
Yurii Bandrivskyi <yuriibandrivskyi@MacBook-Pro.local>
2025-05-19 16:30:54 +0300

    upd v

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -2,6 +2,7 @@ import { wallet } from 'rgb-connect-nodejs';
 
 const xpub = process.env.XPUB;
 const rgbEndpoint = process.env.RGB_MANAGER_ENDPOINT;
+const mnemonic = process.env.MNEMONIC!;
 
 if (!xpub) {
   throw new Error('XPUB is missing from environment variables');
@@ -10,6 +11,6 @@ if (!rgbEndpoint) {
   throw new Error('RGB_MANAGER_ENDPOINT is missing from environment variables');
 }
 
-wallet.init(xpub, rgbEndpoint);
+wallet.init(xpub, rgbEndpoint,mnemonic);
 
 export { wallet };
