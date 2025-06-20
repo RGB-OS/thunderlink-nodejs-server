11ac803a7a72892a0fe0ef2f34387e1225aa98d8
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2025-06-20 18:23:22 +0300

    test sign

--- src/services/webhookService.ts
+++ src/services/webhookService.ts
@@ -5,25 +5,25 @@ const mnemonic = process.env.MNEMONIC!;
 export const confirmWebhook = async (req: any, res: any) => {
 
     try {
-        const { invoice } = req.body;
-        const rgbinvoice = await wallet.decodeRGBInvoice({invoice}) as any;
-        const {asset_id, recipient_id} = rgbinvoice;
-        const invoiceData = {
-             invoice
-        }
+        // const { invoice } = req.body;
+        // const rgbinvoice = await wallet.decodeRGBInvoice({invoice}) as any;
+        // const {asset_id, recipient_id} = rgbinvoice;
+        // const invoiceData = {
+        //      invoice
+        // }
         
-        const psbt = await wallet.sendBegin(invoiceData);
-        console.log('psbt', psbt);
-        const signedPsbt = await wallet.signPsbt(psbt);
-        console.log('signedPsbt', signedPsbt);
-        const finishedPsbt = await wallet.sendEnd({ signed_psbt: signedPsbt });
-        console.log('finishedPsbt', finishedPsbt);
+        // const psbt = await wallet.sendBegin(invoiceData);
+        // console.log('psbt', psbt);
+        // const signedPsbt = await wallet.signPsbt(psbt);
+        // console.log('signedPsbt', signedPsbt);
+        // const finishedPsbt = await wallet.sendEnd({ signed_psbt: signedPsbt });
+        // console.log('finishedPsbt', finishedPsbt);
 
-        const transfers = await wallet.listTransfers(asset_id);
-        const transfer = transfers.find(t => t.recipient_id === recipient_id);
-        if (transfer && InvoiceWatcher.shouldWatch(recipient_id, transfer)) {
-            InvoiceWatcher.startWatcher(recipient_id, asset_id, transfer);
-        }
+        // const transfers = await wallet.listTransfers(asset_id);
+        // const transfer = transfers.find(t => t.recipient_id === recipient_id);
+        // if (transfer && InvoiceWatcher.shouldWatch(recipient_id, transfer)) {
+        //     InvoiceWatcher.startWatcher(recipient_id, asset_id, transfer);
+        // }
 
         return res.json({ message: 'Transfer sent' });
     } catch (error) {
