4dca71c7fd69d1863634012f3e2899ebc4958fd2
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2025-06-24 13:36:17 +0300

    update server

--- src/routes/invoice.ts
+++ src/routes/invoice.ts
@@ -4,7 +4,7 @@ import { auth } from '../middlewares/auth';
 import { apiErrorHandler } from '../utils/apiErrorHandler';
 
 const router = express.Router();
-router.post('/create', auth, apiErrorHandler(createInvoice));
+// router.post('/create', auth, apiErrorHandler(createInvoice));
 router.post('/status', auth, apiErrorHandler(getInvoiceStatus));
 router.post('/decode', auth, apiErrorHandler(decodeRGBInvoice));
 
